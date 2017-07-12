fe9da70f4d35d566d6974d3371ac035275404460
lornatumuhairwe <lornatumuhairwe@gmail.com>
2017-07-12 13:58:47 +0300

    changed home route to login

--- app/views.py
+++ app/views.py
@@ -1,17 +1,17 @@
 from flask import render_template
 from app import app
 
-@app.route('/test')
+@app.route('/test') #my test route
 def testroute():
 	return render_template("inde.html")
+
+@app.route('/')
+def login():
+	return render_template("login.html")
 
 @app.route('/signup')
 def signup():
 	return render_template("signup.html")
-
-@app.route('/login')
-def login():
-	return render_template("login.html")
 
 @app.route('/home')
 def home():
